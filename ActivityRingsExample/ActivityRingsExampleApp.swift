e1f936b7448f4a0ee7ae82b99b8e6231c3e1d1ee
Tom Phillips <tomphillipsmusic@gmail.com>
2023-05-19 08:14:09 -0400

    Initial Commit

---
+++ ActivityRingsExample/ActivityRingsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityRingsExampleApp.swift
+//  ActivityRingsExample
+//
+//  Created by Tom Phillips on 5/19/23.
+//
+
+import SwiftUI
+
+@main
+struct ActivityRingsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
